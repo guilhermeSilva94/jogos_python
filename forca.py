acb7ac5c3ab6d4a288e4059e7b19127fb982b503
Guilherme Silva <guidss3394@gmail.com>
2023-02-07 20:50:37 -0300

    Melhorando mensagens do jogo e acrescentado desenho da forca

--- forca.py
+++ forca.py
@@ -1,6 +1,57 @@
 from random import randrange
 
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
 def jogar():
+    
     print('Bem vindo no jogo de Forca!')
     print('*' * 27)
 
@@ -20,7 +71,6 @@ def jogar():
     #for letra in palavra_secreta:
         #letras_acertadas.append('_')
    
-
     enforcou = False
     acertou = False
     erros = 0
@@ -40,18 +90,50 @@ def jogar():
                 index += 1 
         else:
             erros += 1
+            desenha_forca(erros)
 
-        enforcou = (erros == 6)
+        enforcou = (erros == 7)
         acertou = '_' not in letras_acertadas
 
         print(letras_acertadas)
 
     if (acertou):
-        print("Você ganhou!")
+        print('')
+        print("Parabéns, você ganhou!")
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
     else:
-        print("Você perdeu!")
+        print('')
+        print("Puxa, você foi enforcado!")
+        print("A palavra era {}".format(palavra_secreta))
+        print("    _______________         ")
+        print("   /               \       ")
+        print("  /                 \      ")
+        print("//                   \/\  ")
+        print("\|   XXXX     XXXX   | /   ")
+        print(" |   XXXX     XXXX   |/     ")
+        print(" |   XXX       XXX   |      ")
+        print(" |                   |      ")
+        print(" \__      XXX      __/     ")
+        print("   |\     XXX     /|       ")
+        print("   | |           | |        ")
+        print("   | I I I I I I I |        ")
+        print("   |  I I I I I I  |        ")
+        print("   \_             _/       ")
+        print("     \_         _/         ")
+        print("       \_______/           ")
         print('Fim de jogo')
 
 if (__name__ == '__main__'):
     jogar()
 
+
+ 
